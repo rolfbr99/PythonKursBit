5094b9226fcd1abe9138cb9dc688b13b3aff463f
Rolf Brudermann <rolf@piko.ch>
2021-03-15 15:30:30 +0100

    ok

--- labs/maximum.py
+++ labs/maximum.py
@@ -1,11 +1,8 @@
-def max (a,b,c=None):
-    if c is None:
-        lst=[a,b]
-    else:
-        lst=[a,b,c]    
+def max(*args):
+    lst=list(locals()["args"])
     lst.sort(reverse=True)
     return lst[0]
 
 print(max(1,10,3))
 print(max(3,2))
-print(max(30,10,50))
+print(max(30,10,50,80,10))
